4e11355f6bc4fca62d7d4be9fb75e90fc9ed3278
meytizn <zamanianmehdi23desktop@gmail.com>
2025-12-08 14:32:48 +0330

    project started with using font and image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className={`   `}>
         
+
+<Image
+          className="dark:invert"
+          src="/images/vedavski.jpg"   //it refers to public/
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
 
     </div>
   );
