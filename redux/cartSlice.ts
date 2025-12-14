4d63302a7ce5a3948edafb2b49dcaea455a28133
meytizn <zamanianmehdi23desktop@gmail.com>
2025-12-14 09:37:17 +0330

    cartslice made with typescript successfully

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -1,51 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+// Define a type for the product in the cart
+interface CartProduct {
+    id: number;
+    title: string; // Corrected the typo from 'titlle' to 'title'
+    quantity: number;
+}
 
+// Define the initial state as an array of CartProduct
+const initialState: CartProduct[] = [];
+
+// Create the cart slice
 const cartSlice = createSlice({
-    name:"carts" ,
-    initialState:[] , //{id :1 , titlle:"book" , quantity:1}
-   
-    reducers:{
+    name: "carts",
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<CartProduct>) => {
+            const oldProduct = state.find(product => product.id === action.payload.id);
 
-        addToCart:( state , action )=>{
-
-            let oldProduct = state.find((product)=>{
-                return product.id == action.payload.id
-            })
-
-            if(oldProduct){
-                oldProduct.quantity+=1
+            if (oldProduct) {
+                oldProduct.quantity += 1;
+            } else {
+                state.push({ ...action.payload, quantity: 1 });
             }
+        },
 
-            else{
-                 state.push({...action.payload , quantity:1}) //spop used to add new property quantity
+        removeFromCart: (state, action: PayloadAction<number>) => {
+            return state.filter(product => product.id !== action.payload);
+        },
+
+        changeQuantityProduct: (state, action: PayloadAction<{ id: number; change: number }>) => {
+            const { id, change } = action.payload;
+            const existingProduct = state.find(product => product.id === id);
+
+            if (existingProduct) {
+                existingProduct.quantity += change;
             }
+        },
+
+        clearCart: () => initialState,
+    },
+});
 
 
-        } ,
-
-
-        removeFromCart:(state,action)=>{
-            return state.filter((product)=>product.id !=action.payload)
-        } ,
-
-
-
-
-        changeQuantityProduct:(state,action)=>{
-            let {id,change} = action.payload  // we have to send product id and change num throughtout a btn
-            let exsistedProduct = state.find((product)=>{
-                return product.id == id
-            })
-            exsistedProduct.quantity+=change
-            
-
-        } ,
-
-
-        clearCart:(state)=>[] 
-
-        
-    }
-})
-export default cartSlice
+export default cartSlice;
